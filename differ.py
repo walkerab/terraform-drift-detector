98774303c9f369d44d98854978bb632237f4f99f
Andrew Walker <walkerab.eml@gmail.com>
2022-11-14 20:40:04 -0800

    switching to diff cli command as it allows ignoring whitespace differences

--- differ.py
+++ differ.py
@@ -1,11 +1,25 @@
-import difflib
+from logging import FileHandler
+import subprocess
+import tempfile
 import re
 
 def diff(a: str, b: str) -> str:
-  diff_string = "".join(
-    difflib.unified_diff(
-      a.splitlines(True),
-      b.splitlines(True)
-    )
+  def tempf(contents: str) -> FileHandler:
+    tf = tempfile.NamedTemporaryFile()
+    tf.write(contents.encode())
+    tf.flush()
+    return tf
+  files = list(map(tempf, [a, b]))
+  p = subprocess.run(
+    [
+      "diff",
+      "--ignore-all-space",
+      "--unified=3",
+      files[0].name,
+      files[1].name
+    ],
+    capture_output=True,
+    text=True
   )
-  return re.sub("(.|\n)*@@.*\n", "", diff_string, count = 1)
+  map(lambda tf: tf.close(), files)
+  return re.sub("(.|\n)*@@.*\n", "", p.stdout, count = 1)
--- tests/differ_test.py
+++ tests/differ_test.py
@@ -15,3 +15,12 @@ goodbye
 """
   assert expected_diff == diff(a,b)
 
+def test_empty_diff():
+  assert '' == diff('', '')
+  assert '' == diff('a', 'a')
+
+def test_ignores_whitespace():
+  assert '' == diff('a', 'a ')
+  assert '' == diff('a', ' a')
+  assert '' == diff('    a     ', '  a   ')
+  assert '' == diff('    a     b   ', '  a b  ')
